b5a4f3cf2fe19e6d5174571841a8ee811c72ee66
Wolevir <100397898+Wolevir@users.noreply.github.com>
2022-10-14 11:03:57 +0400

    Add files via upload

---
+++ 3.1.2_spring_boot_vvvv1/boot1/src/main/java/ru/freeomsk/springboot/DAO/UserDAO.java
@@ -0,0 +1,17 @@
+package ru.freeomsk.springboot.DAO;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.freeomsk.springboot.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+@Transactional
+@Repository
+public interface UserDAO extends JpaRepository<User, Long> {
+    @Query("SELECT u from User u join fetch u.roles where u.username = :username")
+    User findByUsername(@Param("username") String username);
+}
+
+
